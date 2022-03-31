dff79f8e61eb573ab964945ab6d2d8b241780513
Michael Watts <hello@michaelwatts.me>
2022-03-31 11:13:21 +0000

    Alias vue import (#6)
    Plus NPM audit fix

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+const path = require('path')
+
 module.exports = {
   css: { extract: false },
+  chainWebpack: (config) => {
+    config.resolve.alias.set(
+      'vue$',
+      path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js')
+    )
+  },
 }
